2986216da6b7105a74ae3f0b7da1a90be87cc25b
Megan Bergner <43555570+meganrosesvg@users.noreply.github.com>
2019-03-11 18:30:37 -0500

    Updated Code
    6:30pm. Edits to js.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 function onReady () {
-  text toDos = [];
-  let toDoId =
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -16,17 +14,17 @@ function onReady () {
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox"; // input <type> checkbox
 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "delete";
+
     newLi.textContent = title;
     newLi.appendChild(checkbox);
     newLi.appendChild(deleteBtn);
     toDoList.appendChild(newLi);
     newToDoText.value = "";
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "delete";
 
     deleteBtn.addEventListener('click', function(event) {
-      let buttonLiText = this.parentElement.childNodes[].textContent;
       //console.log(event)
       //this.parentElement represents the buttons <li> parent
       toDoList.removeChild(this.parentElement);
