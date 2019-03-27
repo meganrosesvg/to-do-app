7fd31cce4a2c4427a5897293fdd93071d39640f9
Megan Bergner <43555570+meganrosesvg@users.noreply.github.com>
2019-03-27 16:52:06 -0500

    Updates
    3/27/19

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 function onReady () {
-  var toDos =[];
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  let toDos = [];
   let id = 0;
+
+  function createNewToDo () {
+    const newToDoText = document.getElementById('newToDoText');
+    if(!newToDoText.value){ return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    id++;
+
+    newToDoText.value = '';
+    renderTheUI();
+  }
+}
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
@@ -13,39 +27,23 @@ function onReady () {
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = "Delete";
-
-        deleteBtn.addEventListener('click', event => {
-            toDos = toDos.filter(function(item){
-              return item.id !== toDo.id;
-            })
-          renderTheUi();
-        });
-
         newLi.textContent = toDo.title;
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
         newLi.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener('click', event => {
+            toDos = toDos.filter(function(item){
+              return item.id !== toDo.id;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+
+        renderTheUi();
     });
+}
 
-  }
-
-  function createNewToDo () {
-    const newToDoText = document.getElementById('newToDoText');
-    if(|newToDoText.value)( return; )
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-      id: id
-    });
-
-    id++;
-
-    newToDoText.value = '';
-    renderTheUI();
-  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -57,4 +55,5 @@ function onReady () {
 
 window.onload = function() {
   onReady();
-};
+  };
+}
